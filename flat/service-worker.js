b958d670a89136ece2d6a3765fa55b375858ac8a
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-07-09 03:09:33 +0000

    Deploy iconduit/demo to github.com/iconduit/demo.git:gh-pages

--- flat/service-worker.js
+++ flat/service-worker.js
@@ -1,6 +1,6 @@
 self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open("cache-1561814553168")
+    caches.open("cache-1562641705109")
       .then(cache => cache.add('.'))
   )
 })
