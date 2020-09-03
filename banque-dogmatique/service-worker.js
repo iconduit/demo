b77508a85b227d69316f509c9191ca45ea131f20
ezzatron <ezzatron@gmail.com>
2020-09-03 07:01:23 +0000

    Deploying to gh-pages from  @ 8c70df0faff3ad8db0394e3410b4696f0a4a533e 🚀

--- banque-dogmatique/service-worker.js
+++ banque-dogmatique/service-worker.js
@@ -1,6 +1,6 @@
 self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open("cache-1591491526025")
+    caches.open("cache-1599116462456")
       .then(cache => cache.add('.'))
   )
 })
--- material/service-worker.js
+++ material/service-worker.js
@@ -1,6 +1,6 @@
 self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open("cache-1591491526025")
+    caches.open("cache-1599116432813")
       .then(cache => cache.add('.'))
   )
 })
